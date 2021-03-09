59baca608b1a034c4dd93a1b7aad05a323970400
ryo-i <iidazz0109@gmail.com>
2021-03-10 05:17:26 +0900

    JS外部化をやめる

--- public/js/404Redirect.js
+++
@@ -1,2 +0,0 @@
-sessionStorage.setItem('path', window.location.pathname);
-window.location.replace('./');
--- public/js/indexRedirect.js
+++
@@ -1,7 +0,0 @@
-const path = sessionStorage.getItem('path');
-console.log(path);
-
-if (path && path === '/react-from-meta-lang/other') {
-  sessionStorage.removeItem('path');
-  window.history.replaceState(null, null, './other');
-}
